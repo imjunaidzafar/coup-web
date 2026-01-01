12f117a310471cd95ced3c32010060d5721f2815
Muhammad Junaid <mjunaid.zafar01@gmail.com>
2026-01-01 12:07:24 +0500

    feat: add landing page components

---
+++ components/landing/index.ts
@@ -0,0 +1,5 @@
+export { HeroSection } from './hero-section';
+export { MessageBadge } from './message-badge';
+export { CloudLayer } from './cloud-layer';
+export { BirdIllustration } from './bird-illustration';
+export { FlyingBirdAnimation } from './flying-bird';
